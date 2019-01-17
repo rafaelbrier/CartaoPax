74b43657edaa6499f95914f4d9322415fb5c2b88
Rafael Brier <rafaelmjf2010@gmail.com>
2019-01-17 19:11:53 -0200

    Perfect So Far

--- src/app/core/pages/news-page/news-page.component.ts
+++ src/app/core/pages/news-page/news-page.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsPageComponent implements OnInit {
 
+  fakeData: string = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+  magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+  laborum.`;
+  readMore: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    if(this.fakeData && this.fakeData.length > 190) {        
+      this.fakeData = this.fakeData.substr(0, 190) + "...";
+      this.readMore = true;
+    }      
   }
 
 }
