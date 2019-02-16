d843473fca61f5daf8d8163253747e0390e8f91d
Rafael Brier <rafaelmjf2010@gmail.com>
2019-02-15 22:55:35 -0200

    Hmmkay

--- src/app/main-page/body/body.component.ts
+++ src/app/main-page/body/body.component.ts
@@ -8,8 +8,11 @@ import { NewsService } from '../../core/services/news.service';
 })
 export class BodyComponent implements OnInit {
 
-  galleryOptions: any;
-  galleryImages: any;
+  clubGalleryOptions: any;
+  clubGalleryImages: any;
+
+  partnersGalleryOptions: any;
+  partnersGalleryImages: any;
 
   newsData: any;
   news: any;
@@ -50,7 +53,7 @@ export class BodyComponent implements OnInit {
 
 
   initGallery() {
-    this.galleryOptions = [
+    this.clubGalleryOptions = [
       {
         "thumbnailsColumns": 6, "thumbnailsPercent": 20, "imagePercent": 100, "thumbnailMargin": 0,
         "thumbnailsMargin": 0, "thumbnailsOrder": 2, "previewFullscreen": true, "previewKeyboardNavigation": true,
@@ -62,7 +65,8 @@ export class BodyComponent implements OnInit {
       { "breakpoint": 300, "width": "100%", "height": "200px", "thumbnailsColumns": 2 }
     ]
 
-    this.galleryImages = [
+
+    this.clubGalleryImages = [
       {
         small: '../assets/images/gallery/gallery1_tn.jpg',
         medium: '../assets/images/gallery/gallery1med.jpg',
@@ -104,6 +108,56 @@ export class BodyComponent implements OnInit {
         big: '../assets/images/gallery/gallery8.jpg'
       },
     ];
+
+    this.partnersGalleryOptions = [
+      { "image": false, "width": "90%", "height": "150px", "thumbnailsPercent": 100,
+        "thumbnailSize": "contain", "thumbnailsColumns": 6, "thumbnailsArrows": true,
+        "preview": false, "previewInfinityMove": true, "thumbnailsArrowsAutoHide": true,
+        "arrowPrevIcon": "fas fa-chevron-circle-left text-success", "arrowNextIcon": "fas fa-chevron-circle-right text-success" },
+      { "breakpoint": 500, "width": "100%" }
+    ]
+
+    this.partnersGalleryImages = [{
+      small: '../assets/images/parceiros/parceiro1.jpg',
+      medium: '../assets/images/parceiros/parceiro1.jpg',
+      big: '../assets/images/parceiros/parceiro1.jpg'
+    },
+    {
+      small: '../assets/images/parceiros/parceiro2.jpg',
+      medium: '../assets/images/parceiros/parceiro2.jpg',
+      big: '../assets/images/parceiros/parceiro2.jpg'
+    },
+    {
+      small: '../assets/images/parceiros/parceiro3.jpg',
+      medium: '../assets/images/parceiros/parceiro3.jpg',
+      big: '../assets/images/parceiros/parceiro3.jpg'
+    },
+    {
+      small: '../assets/images/parceiros/parceiro4.jpg',
+      medium: '../assets/images/parceiros/parceiro4.jpg',
+      big: '../assets/images/parceiros/parceiro4.jpg'
+    },{
+      small: '../assets/images/parceiros/parceiro1.jpg',
+      medium: '../assets/images/parceiros/parceiro1.jpg',
+      big: '../assets/images/parceiros/parceiro1.jpg'
+    },
+    {
+      small: '../assets/images/parceiros/parceiro2.jpg',
+      medium: '../assets/images/parceiros/parceiro2.jpg',
+      big: '../assets/images/parceiros/parceiro2.jpg'
+    },
+    {
+      small: '../assets/images/parceiros/parceiro3.jpg',
+      medium: '../assets/images/parceiros/parceiro3.jpg',
+      big: '../assets/images/parceiros/parceiro3.jpg'
+    },
+    {
+      small: '../assets/images/parceiros/parceiro4.jpg',
+      medium: '../assets/images/parceiros/parceiro4.jpg',
+      big: '../assets/images/parceiros/parceiro4.jpg'
+    }];
+
   }
+
 
 }
